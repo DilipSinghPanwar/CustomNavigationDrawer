d6528fa15a5ccb973f624751e83656022fcc1ac2
DilipSinghPanwar <dilip.dsp@gmail.com>
2020-06-19 19:57:22 +0530

    Initial commit

---
+++ app/src/main/java/com/fullnavigationdrawer/MainActivity.java
@@ -0,0 +1,35 @@
+package com.fullnavigationdrawer;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        final LinearLayout content = findViewById(R.id.content);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close) {
+            private float scaleFactor = 6f;
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                super.onDrawerSlide(drawerView, slideOffset);
+                float slideX = drawerView.getWidth() * slideOffset;
+                content.setTranslationX(slideX);
+                content.setScaleX(1 - (slideOffset / scaleFactor));
+                content.setScaleY(1 - (slideOffset / scaleFactor));
+            }
+        };
+        drawerLayout.setScrimColor(Color.TRANSPARENT);
+        drawerLayout.setDrawerElevation(0f);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+    }
+}
